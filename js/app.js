aeed8a3686a0785e534a15926cb7293324762814
Jeremy Holland <jeremy_holland@Jeremy-Hollands-iMac.local>
2016-02-12 18:09:58 -0600

    added ui-router

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-var myApp =  angular.module('myApp', []);
+var myApp =  angular.module('myApp', ['ui.router']);
 
-
+myApp.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('home', {
+				controller: 'MainCtrl',
+				url: '/home',
+				templateUrl: 'views/test.html'
+			})
+			$urlRouterProvider.otherwise('home');
+	}
+])
 
 
 
 myApp.controller('MainCtrl', ['$scope', function($scope){
+		$scope.test = "what's up, breh"
 
-	
 
 
 }])
